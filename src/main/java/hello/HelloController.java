4a711273256b6a1714493abe17ec8229ab223fb2
Raghav <raghav.15jics089@jietjodhpur.ac.in>
2021-07-23 15:17:22 +0530

    Change in Controller

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
         public String index() {
-            return "Hello From HELM\n";
+            return "Hello Pranav\n";
         }
     @RequestMapping("/hello")
         public String index2() {
